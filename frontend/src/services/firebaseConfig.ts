e50d9860ed4eea50332e382cf1a48550362c06f5
RCburak <kaya.burak.ibrahim42@gmail.com>
2026-02-03 01:48:44 +0300

    entegre

--- frontend/src/services/firebaseConfig.ts
+++ frontend/src/services/firebaseConfig.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, initializeAuth } from "firebase/auth";
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBePH21Qu6U6EJF3IrFbPukMT_xW-LXZ7I",
@@ -13,19 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
-let authInstance: any;
-
-try {
-  // TypeScript hatasını (2305) aşmak için fonksiyonu dinamik olarak çağırıyoruz
-  const { getReactNativePersistence } = require("firebase/auth"); 
-  
-  authInstance = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-  });
-} catch (e) {
-  // Eğer zaten başlatılmışsa veya hata verirse mevcut auth'u al
-  authInstance = getAuth(app);
-}
-
-export const auth = authInstance;
+export const auth = getAuth(app);
